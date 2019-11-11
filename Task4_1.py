f43734413894df0cfb4cb88ba800419a9eea966a
Liliia Maystrenko <liliiaboiko@mail.com>
2019-11-11 02:29:49 +0200

    Added HW Task4_1 and Task4_2

--- Task4_1.py
+++
@@ -1,9 +0,0 @@
-def zero_fuel(distance_to_pump, mpg, fuel_left):
-    #Happy Coding! ;)
-#     distance_to_punm = 50
-#     mpg = 25
-#     fuel_left = 2
-    if fuel_left * mpg >= distance_to_pump:
-        return True
-    else:
-        return False
--- Task4_2.py
+++
@@ -1,7 +0,0 @@
-def enough(cap, on, wait):
-    # Your code here
-    if wait > cap - on:
-        return wait - (cap - on)
-    else:
-        return 0
-        print("Can fit all passengers")
