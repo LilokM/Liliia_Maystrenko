a482289a87b578802b7ac559e00f0de303408a01
Liliia Maystrenko <liliiaboiko@mail.com>
2019-11-17 23:30:24 +0200

    Added codewars tasks: 4_3, 4_4, 4_5, 4_6

---
+++ Task4_5_counting_sheep.py
@@ -0,0 +1,8 @@
+def count_sheeps(arrayOfSheeps):
+  # TODO May the force be with you
+  count_num = 0
+  for sheep in arrayOfSheeps:
+      if sheep == True:
+          count_num = count_num +1
+          pass
+  return count_num
---
+++ Task4_6_is_this_my_tail.py
@@ -0,0 +1,6 @@
+def correct_tail(body, tail):
+    sub = body[-1]
+    if tail == sub:
+        return True
+    else:
+        return False
