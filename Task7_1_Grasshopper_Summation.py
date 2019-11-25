6408d9ab2d8d0a1cda142405a39b288a35c22461
Liliia Maystrenko <liliiaboiko@mail.com>
2019-11-26 00:31:51 +0200

    Add Task6_4_Grasshopper_Summation, Task7_1_Grasshopper_Summation, Task7_2_Fix_the_loop, Task7_3_Double_Char.py

---
+++ Task7_1_Grasshopper_Summation.py
@@ -0,0 +1,7 @@
+def summation(num):
+#     pass # Code here
+    temp_num = 1
+    while num > 1:
+        temp_num += num
+        num -= 1
+    return temp_num
---
+++ Task7_3_Double_Char.py
@@ -0,0 +1,3 @@
+def double_char(s):
+#     pass
+    return ''.join([char * 2 for char in s])
